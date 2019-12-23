ca3802c4c3a041f0455b6bcd55513b7363452f0f
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-12-23 20:14:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,60 +1,13 @@
-class Shoe 
-  
-  BRANDS = []
-  
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand 
-  
-  def initialize(brand)
-    @brand = brand 
-    BRANDS << brand unless BRAND.include?(brand)
-  end   
-  
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
-end   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # class Shoe 
-#   attr_accessor :color, :size, :material, :condition
-#   attr_reader :brand
   
 #   BRANDS = []
   
+#   attr_accessor :color, :size, :material, :condition
+#   attr_reader :brand 
+  
 #   def initialize(brand)
 #     @brand = brand 
-#     BRANDS << brand unless BRANDS.include?(brand)
+#     BRANDS << brand unless BRAND.include?(brand)
 #   end   
   
 #   def cobble
@@ -62,3 +15,50 @@ end
 #     puts "Your shoe is as good as new!"
 #   end
 # end   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Shoe 
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
+  BRANDS = []
+  
+  def initialize(brand)
+    @brand = brand 
+    BRANDS << brand unless BRANDS.include?(brand)
+  end   
+  
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end   
