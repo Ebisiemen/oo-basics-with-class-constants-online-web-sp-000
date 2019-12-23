2d0f44f0ef7096f0f988943296771c9086423c78
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-12-23 20:15:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,6 +5,8 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand 
   
+  BRANDS = []
+  
   def initialize(brand)
     @brand = brand 
     BRANDS << brand unless BRAND.include?(brand)
