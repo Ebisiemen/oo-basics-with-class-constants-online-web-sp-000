c634730add7f23268d0bac7d654ed81d6b70bebf
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-12-23 19:53:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,48 @@
-class Shoe 
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Shoe 
+#   attr_accessor :color, :size, :material, :condition
+#   attr_reader :brand
   
-  BRANDS = []
+#   BRANDS = []
   
-  def initialize(brand)
-    @brand = brand 
-    BRANDS << brand unless BRANDS.include?(brand)
-  end   
+#   def initialize(brand)
+#     @brand = brand 
+#     BRANDS << brand unless BRANDS.include?(brand)
+#   end   
   
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
-end   
+#   def cobble
+#     self.condition = "new"
+#     puts "Your shoe is as good as new!"
+#   end
+# end   
